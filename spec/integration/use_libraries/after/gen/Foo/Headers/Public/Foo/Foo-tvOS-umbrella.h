b4045ead5cda983125b72347bf6f9b7fc8b9d7cd
Samuel Giddins <segiddins@squareup.com>
2019-03-08 18:29:18 -0800

    Update integration specs for cocoapods 1.7

--- spec/integration/use_libraries/after/gen/Foo/Headers/Public/Foo/Foo-tvOS-umbrella.h
+++ spec/integration/use_libraries/after/gen/Foo/Headers/Public/Foo/Foo-tvOS-umbrella.h
@@ -1,1 +1,1 @@
-/Users/derko/Development/cocoapods/cocoapods-generate/spec/integration/tmp/use_libraries/transformed/gen/Foo/Target Support Files/Foo-tvOS/Foo-tvOS-umbrella.h
+../../../Target Support Files/Foo-tvOS/Foo-tvOS-umbrella.h
--- spec/integration/use_libraries/after/gen/Foo/Headers/Public/Foo/Foo-watchOS-umbrella.h
+++ spec/integration/use_libraries/after/gen/Foo/Headers/Public/Foo/Foo-watchOS-umbrella.h
@@ -1,1 +1,1 @@
-/Users/derko/Development/cocoapods/cocoapods-generate/spec/integration/tmp/use_libraries/transformed/gen/Foo/Target Support Files/Foo-watchOS/Foo-watchOS-umbrella.h
+../../../Target Support Files/Foo-watchOS/Foo-watchOS-umbrella.h
