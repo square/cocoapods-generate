6d17010c447612e231ad3b0b121688c0a15ce47d
Paul Beusterien <paulbeusterien@google.com>
2019-04-26 16:40:54 -0700

    Add integration test

---
+++ spec/integration/local_sources/after/gen/E/Target Support Files/B-macOS/B-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_B_macOS : NSObject
+@end
+@implementation PodsDummy_B_macOS
+@end
---
+++ spec/integration/local_sources/before/Pods/Target Support Files/C/C-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_C : NSObject
+@end
+@implementation PodsDummy_C
+@end
